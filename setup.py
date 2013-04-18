ec2de46c0928b5bfd430aab212fc48e17f93edf2
Mitch Garnaat <mitch@garnaat.com>
2013-04-18 07:04:02 -0700

    Initial version plucked from aws-cli.

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script.
+"""
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'bcdoc',
+]
+
+requires = ['six>=1.1.0']
+
+
+setup(
+    name='bcdoc',
+    version='0.1',
+    description='ReST document generation tools for botocore.',
+    long_description=open('README.md').read(),
+    author='Mitch Garnaat',
+    author_email='mitch@garnaat.com',
+    url='https://github.com/botocore/bcdoc',
+    packages=packages,
+    package_dir={'bcdoc': 'bcdoc'},
+    install_requires=requires,
+    license=open("LICENSE.txt").read(),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+    ),
+)
