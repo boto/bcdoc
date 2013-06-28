affffcb16b85a7fd1ce0a05d27fff7bed7fa3b0a
Mitch Garnaat <mitch@garnaat.com>
2013-06-28 16:36:25 -0700

    More refactoring (and hopefully simplification) of the events.

--- bcdoc/clidocevents.py
+++ bcdoc/clidocevents.py
@@ -11,143 +11,132 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from botocore.provider import Provider
-from botocore.service import Service
-from botocore.operation import Operation
 
 
-class CLIDocEvents(object):
+DOC_EVENTS = {
+    'doc-title': '.%s.%s',
+    'doc-description': '.%s.%s',
+    'doc-synopsis-start': '.%s.%s',
+    'doc-synopsis-option': '.%s.%s.%s',
+    'doc-synopsis-end': '.%s.%s',
+    'doc-options-start': '.%s.%s',
+    'doc-option': '.%s.%s.%s',
+    'doc-option-example': '.%s.%s.%s',
+    'doc-options-end': '.%s.%s',
+    'doc-examples': '.%s.%s',
+    'doc-subitems-start': '.%s.%s',
+    'doc-subitem': '.%s.%s.%s',
+    'doc-subitems-end': '.%s.%s',
+    }
 
-    DocEvents = {
-        'doc-title': '.%s.%s',
-        'doc-description': '.%s.%s',
-        'doc-synopsis-start': '.%s.%s',
-        'doc-synopsis-option': '.%s.%s.%s',
-        'doc-synopsis-end': '.%s.%s',
-        'doc-options-start': '.%s.%s',
-        'doc-option': '.%s.%s.%s',
-        'doc-option-example': '.%s.%s.%s',
-        'doc-options-end': '.%s.%s',
-        'doc-examples': '.%s.%s',
-        'doc-subitems-start': '.%s.%s',
-        'doc-subitem': '.%s.%s.%s',
-        'doc-subitems-end': '.%s.%s',
-        }
+def fire_event(session, event_name, *fmtargs, **kwargs):
+    event = session.create_event(event_name, *fmtargs)
+    session.emit(event, **kwargs)
 
-    def __init__(self, session):
-        self.session = session
+def document_provider(session, help_command):
+    provider = help_command.provider
+    fire_event(session, 'doc-title', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    fire_event(session, 'doc-description', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    fire_event(session, 'doc-synopsis-start', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    fire_event(session, 'doc-synopsis-end', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    fire_event(session, 'doc-options-start', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    for arg_name in help_command.arg_table:
+        arg = help_command.arg_table[arg_name]
+        fire_event(session,'doc-option', 'Provider', provider.name,
+                   arg_name, argument=arg, help_command=help_command)
+        fire_event(session, 'doc-option-example', 'Provider',
+                   provider.name, arg_name, argument=arg,
+                   help_command=help_command)
+    fire_event(session, 'doc-options-end', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    fire_event(session, 'doc-subitems-start', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
+    for service_name in help_command.command_table.keys():
+        fire_event(session, 'doc-subitem', 'Provider',
+                   provider.name, service_name,
+                   provider=provider, service_name=service_name,
+                   help_command=help_command)
+    fire_event(session, 'doc-subitems-end', 'Provider', provider.name,
+               provider=provider, help_command=help_command)
 
-    def fire_event(self, event_name, *fmtargs, **kwargs):
-        event = self.session.create_event(event_name, *fmtargs)
-        self.session.emit(event, **kwargs)
+def document_service(session, help_command):
+    service = help_command.service
+    fire_event(session, 'doc-title', 'Service', service.endpoint_prefix,
+               service=service, help_command=help_command)
+    fire_event(session, 'doc-description', 'Service', service.endpoint_prefix,
+               service=service, help_command=help_command)
+    fire_event(session, 'doc-synopsis-start', 'Service',
+               service.endpoint_prefix,
+               service=service, help_command=help_command)
+    fire_event(session, 'doc-synopsis-end', 'Service',
+               service.endpoint_prefix,
+               service=service, help_command=help_command)
+    fire_event(session, 'doc-options-start', 'Service',
+               service.endpoint_prefix,
+               service=service, help_command=help_command)
+    fire_event(session, 'doc-options-end', 'Service',
+               service.endpoint_prefix,
+               service=service, help_command=help_command)
+    fire_event(session, 'doc-subitems-start', 'Service',
+               service.endpoint_prefix,
+               service=service, help_command=help_command)
+    for operation_name in help_command.command_table.keys():
+        fire_event(session, 'doc-subitem', 'Service',
+                   service.endpoint_prefix, operation_name,
+                   service=service, operation_name=operation_name,
+                   help_command=help_command)
+    fire_event(session, 'doc-subitems-end', 'Service',
+               service.endpoint_prefix,
+               service=service, help_command=help_command)
 
-    def document_provider(self, provider, help_command):
-        self.fire_event('doc-title', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        self.fire_event('doc-description', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        self.fire_event('doc-synopsis-start', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        self.fire_event('doc-synopsis-end', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        self.fire_event('doc-options-start', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        for arg_name in help_command.arg_table:
-            arg = help_command.arg_table[arg_name]
-            self.fire_event('doc-option', 'Provider', provider.name,
-                            arg_name, argument=arg,
-                            help_command=help_command)
-            self.fire_event('doc-option-example', 'Provider',
-                            provider.name, arg_name, argument=arg,
-                            help_command=help_command)
-        self.fire_event('doc-options-end', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        self.fire_event('doc-subitems-start', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
-        for service_name in help_command.command_table.keys():
-            self.fire_event('doc-subitem', 'Provider',
-                            provider.name, service_name,
-                            provider=provider, service_name=service_name,
-                            help_command=help_command)
-        self.fire_event('doc-subitems-end', 'Provider', provider.name,
-                        provider=provider, help_command=help_command)
+def document_command(session, help_command):
+    command = help_command.command
+    fire_event(session, 'doc-title', 'Command', command.name,
+               command=command, help_command=help_command)
+    fire_event(session, 'doc-description', 'Command', command.name,
+               command=command, help_command=help_command)
 
-    def document_service(self, service, help_command):
-        service = help_command.service
-        self.fire_event('doc-title', 'Service', service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        self.fire_event('doc-description', 'Service', service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        self.fire_event('doc-synopsis-start', 'Service',
-                        service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        self.fire_event('doc-synopsis-end', 'Service',
-                        service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        self.fire_event('doc-options-start', 'Service',
-                        service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        self.fire_event('doc-options-end', 'Service',
-                        service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        self.fire_event('doc-subitems-start', 'Service',
-                        service.endpoint_prefix,
-                        service=service, help_command=help_command)
-        for operation_name in help_command.command_table.keys():
-            self.fire_event('doc-subitem', 'Service',
-                            service.endpoint_prefix, operation_name,
-                            service=service, operation_name=operation_name,
-                            help_command=help_command)
-        self.fire_event('doc-subitems-end', 'Service',
-                        service.endpoint_prefix,
-                        service=service, help_command=help_command)
-
-    def document_operation(self, operation, help_command):
-        self.fire_event('doc-title', 'Operation', operation.name,
-                        operation=operation, help_command=help_command)
-        self.fire_event('doc-description', 'Operation', operation.name,
-                        operation=operation, help_command=help_command)
-        self.fire_event('doc-synopsis-start', 'Operation', operation.name,
-                        operation=operation, help_command=help_command)
-        for arg_name in help_command.arg_table:
-            arg = help_command.arg_table[arg_name]
-            self.fire_event('doc-synopsis-option', 'Operation',
-                            operation.name, arg_name,
-                            operation=operation, argument=arg,
-                            help_command=help_command)
-        self.fire_event('doc-synopsis-end', 'Operation', operation.name,
-                        operation=operation, help_command=help_command)
-        self.fire_event('doc-options-start', 'Operation', operation.name,
-                        operation=operation, help_command=help_command)
-        for arg_name in help_command.arg_table:
-            arg = help_command.arg_table[arg_name]
-            self.fire_event('doc-option', 'Operation',
-                            operation.name, arg_name,
-                            operation=operation, argument=arg,
-                            help_command=help_command)
-            self.fire_event('doc-option-example',  'Operation',
-                            operation.name, arg_name,
-                            operation=operation, argument=arg,
-                            param_shorthand=help_command.param_shorthand,
-                            help_command=help_command)
-        self.fire_event('doc-options-end', 'Operation', operation.name,
-                        operation=operation, help_command=help_command)
-        self.fire_event('doc-examples', 'Operation',
-                        operation.name, operation=operation,
-                        help_command=help_command)
-
-    def register_events(self):
-        for event_name in self.DocEvents:
-            self.session.register_event(event_name,
-                                        self.DocEvents[event_name])
+def document_operation(session, help_command):
+    operation = help_command.operation
+    fire_event(session, 'doc-title', 'Operation', operation.name,
+               operation=operation, help_command=help_command)
+    fire_event(session, 'doc-description', 'Operation', operation.name,
+               operation=operation, help_command=help_command)
+    fire_event(session, 'doc-synopsis-start', 'Operation', operation.name,
+               operation=operation, help_command=help_command)
+    for arg_name in help_command.arg_table:
+        arg = help_command.arg_table[arg_name]
+        fire_event(session, 'doc-synopsis-option', 'Operation',
+                   operation.name, arg_name, operation=operation,
+                   argument=arg, help_command=help_command)
+    fire_event(session, 'doc-synopsis-end', 'Operation', operation.name,
+               operation=operation, help_command=help_command)
+    fire_event(session, 'doc-options-start', 'Operation', operation.name,
+               operation=operation, help_command=help_command)
+    for arg_name in help_command.arg_table:
+        arg = help_command.arg_table[arg_name]
+        fire_event(session, 'doc-option', 'Operation',
+                   operation.name, arg_name,
+                   operation=operation, argument=arg,
+                   help_command=help_command)
+        fire_event(session, 'doc-option-example',  'Operation',
+                   operation.name, arg_name,
+                   operation=operation, argument=arg,
+                   param_shorthand=help_command.param_shorthand,
+                   help_command=help_command)
+    fire_event(session, 'doc-options-end', 'Operation', operation.name,
+               operation=operation, help_command=help_command)
+    fire_event(session, 'doc-examples', 'Operation',
+               operation.name, operation=operation,
+               help_command=help_command)
 
 
-def document(session, obj, help_command, **kwargs):
-    doc_events = CLIDocEvents(session)
-    doc_events.register_events()
-    if isinstance(obj, Provider):
-        doc_events.document_provider(obj, help_command, **kwargs)
-    elif isinstance(obj, Service):
-        doc_events.document_service(obj, help_command, **kwargs)
-    elif isinstance(obj, Operation):
-        doc_events.document_operation(obj, help_command, **kwargs)
+def register_events(session):
+    for event_name in DOC_EVENTS:
+        session.register_event(event_name,
+                               DOC_EVENTS[event_name])
