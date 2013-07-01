3bcd44961b9406d182f46081db0cf6593ae74846
Mitch Garnaat <mitch@garnaat.com>
2013-07-01 10:07:36 -0700

    Further simplification of doc event handlers.  The initialize method in the base class now handles all standard registration.  Subclasses just need to implement methods named after the event they want to handle (e.g. doc-title -> def doc_title(self, ...)).

--- bcdoc/clidocs.py
+++ bcdoc/clidocs.py
@@ -15,6 +15,7 @@ from six.moves import cStringIO
 from botocore.hooks import HierarchicalEmitter
 from bcdoc.docstringparser import DocStringParser
 from bcdoc.style import ReSTStyle
+from bcdoc.clidocevents import DOC_EVENTS
 
 LOG = logging.getLogger(__name__)
 
@@ -62,36 +63,44 @@ class ReSTDocument(object):
 
 class CLIDocumentEventHandler(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, help_command):
+        self.help_command = help_command
+        self.initialize(help_command.session, help_command.event_class)
 
-    def initialize(self, session):
-        pass
+    def initialize(self, session, event_class):
+        """
+        The default initialization iterates through all of the
+        available document events and looks for a corresponding
+        handler method defined in the object.  If it's there, that
+        handler method will be registered for the all events of
+        that type for the specified ``event_class``.
+        """
+        for event in DOC_EVENTS:
+            event_handler_name = event.replace('-', '_')
+            if hasattr(self, event_handler_name):
+                LOG.debug('found event_handler: %s' % event_handler_name)
+                event_handler = getattr(self, event_handler_name)
+                format_string = DOC_EVENTS[event]
+                num_args = len(format_string.split('.')) - 2
+                format_args = (event_class,) + ('*',) * num_args
+                event_string = event + format_string % format_args
+                LOG.debug('about to register: %s' % event_string)
+                session.register(event_string, event_handler)
 
 
 class ProviderDocumentEventHandler(CLIDocumentEventHandler):
 
-    def initialize(self, session):
-        CLIDocumentEventHandler.initialize(self, session)
-        session.register('doc-title.Provider.*', self.title)
-        session.register('doc-description.Provider.*', self.description)
-        session.register('doc-synopsis-start.Provider.*', self.synopsis)
-        session.register('doc-options-start.Provider.*', self.options)
-        session.register('doc-option.Provider.*.*', self.option)
-        session.register('doc-subitems-start.Provider.*', self.subitems)
-        session.register('doc-subitem.Provider.*.*', self.subitem)
-
-    def title(self, help_command, **kwargs):
+    def doc_title(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h1(help_command.name)
 
-    def description(self, help_command, **kwargs):
+    def doc_description(self, help_command, **kwargs):
         doc = help_command.doc
         provider = help_command.obj
         doc.style.h2('Description')
         doc.include_doc_string(help_command.description)
 
-    def synopsis(self, help_command, **kwargs):
+    def doc_synopsis(self, help_command, **kwargs):
         doc = help_command.doc
         provider = help_command.obj
         doc.style.h2('Synopsis')
@@ -99,11 +108,11 @@ class ProviderDocumentEventHandler(CLIDocumentEventHandler):
         doc.include_doc_string(help_command.help_usage)
         doc.style.new_paragraph()
 
-    def options(self, help_command, **kwargs):
+    def doc_options(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h2('Options')
 
-    def option(self, arg_name, help_command, **kwargs):
+    def doc_option(self, arg_name, help_command, **kwargs):
         doc = help_command.doc
         argument = help_command.arg_table[arg_name]
         doc.write('``%s`` (%s)\n' % (argument.cli_name,
@@ -118,12 +127,12 @@ class ProviderDocumentEventHandler(CLIDocumentEventHandler):
                 doc.style.li(choice)
             doc.style.end_ul()
 
-    def subitems(self, help_command, **kwargs):
+    def doc_subitems(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h2('Available Services')
         doc.style.toctree()
 
-    def subitem(self, command_name, help_command, **kwargs):
+    def doc_subitem(self, command_name, help_command, **kwargs):
         doc = help_command.doc
         if doc.target == 'man':
             doc.write('* %s\n' % command_name)
@@ -133,33 +142,26 @@ class ProviderDocumentEventHandler(CLIDocumentEventHandler):
 
 class ServiceDocumentEventHandler(CLIDocumentEventHandler):
 
-    def initialize(self, session):
-        CLIDocumentEventHandler.initialize(self, session)
-        session.register('doc-title.Service.*', self.title)
-        session.register('doc-description.Service.*', self.description)
-        session.register('doc-subitems-start.Service.*', self.subitems)
-        session.register('doc-subitem.Service.*.*', self.subitem)
-
     def build_translation_map(self):
         for op in self.service.operations:
             self.translation_map[op.name] = op.cli_name
 
-    def title(self, help_command, **kwargs):
+    def doc_title(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h1(help_command.name)
 
-    def description(self, help_command, **kwargs):
+    def doc_description(self, help_command, **kwargs):
         doc = help_command.doc
         service = help_command.obj
         doc.style.h2('Description')
         doc.include_doc_string(service.documentation)
 
-    def subitems(self, help_command, **kwargs):
+    def doc_subitems(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h2('Available Commands')
         doc.style.toctree()
 
-    def subitem(self, command_name, help_command, **kwargs):
+    def doc_subitem(self, command_name, help_command, **kwargs):
         doc = help_command.doc
         if doc.target == 'man':
             doc.write('* %s\n' % command_name)
@@ -169,44 +171,29 @@ class ServiceDocumentEventHandler(CLIDocumentEventHandler):
 
 class OperationDocumentEventHandler(CLIDocumentEventHandler):
 
-    def initialize(self, session):
-        CLIDocumentEventHandler.initialize(self, session)
-        session.register('doc-title.Operation.*', self.title)
-        session.register('doc-description.Operation.*', self.description)
-        session.register('doc-synopsis-start.Operation.*',
-                         self.synopsis_start)
-        session.register('doc-synopsis-option.Operation.*.*',
-                         self.synopsis_option)
-        session.register('doc-synopsis-end.Operation.*',
-                         self.synopsis_end)
-        session.register('doc-options-start.Operation.*', self.options)
-        session.register('doc-option.Operation.*.*', self.option)
-        session.register('doc-option-example.Operation.*.*',
-                         self.example_shorthand)
-
     def build_translation_map(self):
         for param in self.operation.params:
             self.translation_map[param.name] = param.cli_name
         for operation in self.operation.service.operations:
             self.translation_map[operation.name] = operation.cli_name
 
-    def title(self, help_command, **kwargs):
+    def doc_title(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h1(help_command.name)
 
-    def description(self, help_command, **kwargs):
+    def doc_description(self, help_command, **kwargs):
         doc = help_command.doc
         operation = help_command.obj
         doc.style.h2('Description')
         doc.include_doc_string(operation.documentation)
 
-    def synopsis_start(self, help_command, **kwargs):
+    def doc_synopsis_start(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.h2('Synopsis')
         doc.style.start_codeblock()
         doc.writeln('%s' % help_command.name)
 
-    def synopsis_option(self, arg_name, help_command, **kwargs):
+    def doc_synopsis_option(self, arg_name, help_command, **kwargs):
         doc = help_command.doc
         argument = help_command.arg_table[arg_name]
         option_str = argument.cli_name
@@ -216,18 +203,18 @@ class OperationDocumentEventHandler(CLIDocumentEventHandler):
             option_str = '[%s]' % option_str
         doc.writeln('%s' % option_str)
 
-    def synopsis_end(self, help_command, **kwargs):
+    def doc_synopsis_end(self, help_command, **kwargs):
         doc = help_command.doc
         doc.style.end_codeblock()
 
-    def options(self, help_command, **kwargs):
+    def doc_options(self, help_command, **kwargs):
         doc = help_command.doc
         operation = help_command.obj
         doc.style.h2('Options')
         if len(operation.params) == 0:
             doc.write('*None*\n')
 
-    def option(self, arg_name, help_command, **kwargs):
+    def doc_option(self, arg_name, help_command, **kwargs):
         doc = help_command.doc
         argument = help_command.arg_table[arg_name]
         doc.write('``%s`` (%s)\n' % (argument.cli_name,
@@ -237,7 +224,7 @@ class OperationDocumentEventHandler(CLIDocumentEventHandler):
         doc.style.dedent()
         doc.style.new_paragraph()
 
-    def example_shorthand(self, arg_name, help_command, **kwargs):
+    def doc_example_shorthand(self, arg_name, help_command, **kwargs):
         doc = help_command.doc
         argument = help_command.arg_table[arg_name]
         param = argument.argument_object
